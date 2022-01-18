d7245f76bf66e57a4e1424e5f624b8f0ad738af2
react-rxjs <satng@qq.com>
2022-01-18 22:29:06 +0800

    remove x5

--- android/src/main/java/com/webview/filereader/FlutterFileReaderPlugin.java
+++ android/src/main/java/com/webview/filereader/FlutterFileReaderPlugin.java
@@ -1,15 +1,10 @@
 package com.webview.filereader;
 
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Build;
 import android.content.Context;
-import android.content.IntentFilter;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
-import android.util.Log;
 
-
-import java.lang.reflect.Field;
-import java.util.HashMap;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
@@ -17,21 +12,55 @@ import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+
 
 /**
  * FlutterX5Plugin
  */
-public class FlutterFileReaderPlugin implements FlutterPlugin {
+public class FlutterFileReaderPlugin implements FlutterPlugin, ActivityAware, MethodCallHandler {
 
     public static final String channelName = "wv.io/FileReader";
-    private Context ctx;
+    Activity activity;
+    private Context applicationContext;
+    private MethodChannel methodChannel;
 
+    @Override
+    public void onAttachedToEngine(FlutterPluginBinding binding) {
+        onAttachedToEngine(binding.getApplicationContext(), binding.getBinaryMessenger());
+    }
+    private void onAttachedToEngine(Context applicationContext, BinaryMessenger messenger) {
+        this.applicationContext = applicationContext;
+        methodChannel = new MethodChannel(messenger, channelName);
+        methodChannel.setMethodCallHandler(this);
+    }
+    @Override
+    public void onAttachedToActivity(ActivityPluginBinding binding) {
+        this.activity = binding.getActivity();
+    }
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
 
-    private void init(Context context, BinaryMessenger messenger) {
-        ctx = context;
     }
 
+    @Override
+    public void onReattachedToActivityForConfigChanges( ActivityPluginBinding binding) {
 
+    }
+    @Override
+    public void onDetachedFromActivity() {
+        this.activity = null;
+    }
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+        result.success(null);
+    }
+
+    @Override
+    public void onDetachedFromEngine(FlutterPluginBinding binding) {
+        applicationContext = null;
+        methodChannel.setMethodCallHandler(null);
+    }
 }
 
